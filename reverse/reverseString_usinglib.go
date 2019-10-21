10ffaa88428dd6b0515ae224dbeb18a1aa6276d9
root <root@ip-172-31-94-188.ec2.internal>
2019-10-21 06:47:18 +0000

    go install and more

--- reverse/reverseString_usinglib.go
+++ reverse/reverseString_usinglib.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	var s string = "Milind"
+	var s string
+	fmt.Println("Enter the string to be reversed")
+
+	fmt.Scanf("%s", &s)
+
 	//userlib.reverseStringlib.reverse(s)
 	//userlib.Reverse(s)
 	fmt.Println(userlib.Reverse(s))
