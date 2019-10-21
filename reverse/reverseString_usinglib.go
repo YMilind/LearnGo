61b9f7c60d2da0ce5828349d4ac8c93540ad5da6
root <root@ip-172-31-94-188.ec2.internal>
2019-10-21 06:33:18 +0000

    learned go packing : go install

---
+++ reverse/reverseString_usinglib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"userlib"
+)
+
+func main() {
+	var s string = "Milind"
+	//userlib.reverseStringlib.reverse(s)
+	//userlib.Reverse(s)
+	fmt.Println(userlib.Reverse(s))
+}
