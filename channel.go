d450b45248d6b50c80d7c8dc93042e797431edf3
Milind <milind24365@gmail.com>
2019-10-23 08:05:44 +0000

    for with channel

--- channel.go
+++ channel.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var length int
+var name string
+	fmt.Println("Enter the queue length")
+	fmt.Scanf("%d", &length)
+	queue := make(chan string, length)
+	for q := 0; q < length; q++ {
+		fmt.Println("Enter names")
+		fmt.Scanf("%s", &name)
+		queue <- name
 
-	queue := make(chan string, 3)
-	queue <- "one"
-	queue <- "two"
-	queue <- "3"
+	}
 	close(queue)
-
 	for elem := range queue {
 		fmt.Println(elem)
 	}
