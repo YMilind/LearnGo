ff956d479ddafa6241d1e31ac6e2a0b36173f6be
Milind <milind24365@gmail.com>
2019-10-24 06:49:01 +0000

    String lenght and functions

---
+++ stringsOperation.go
@@ -0,0 +1,13 @@
+package main
+
+import  "fmt"
+
+func main() {
+
+        var newString string = "what's up"
+ fmt.Println("enter string to find length ..")
+fmt.Scanf("%s",&newString)
+fmt.Println("Length of string is..")
+
+        fmt.Println(len(newString))
+}
